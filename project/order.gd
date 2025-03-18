392f0d396333363fb804c030fd6d07fe90f9115d
rithikabakshi <rithikabakshi@gmail.com>
2025-03-18 12:13:39 -0500

    butt

--- project/order.gd
+++ project/order.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
 
-func _on_button_4_pressed() -> void:
-	get_tree().change_scene_to_file("res://order.tscn")  # Replace with function body.
+
+func _on_order_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://order.tscn")
 
 
-func _on_button_3_pressed() -> void:
+func _on_roll_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://roll.tscn")
 
 
-func _on_button_2_pressed() -> void:
+func _on_cook_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://cook.tscn")
 
 
-func _on_button_pressed() -> void:
+func _on_curry_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://curry.tscn")
