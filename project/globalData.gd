687526c97af300a13468d1bb8cb85db3e72d2c5d
trishadar <trisha.darure@gmail.com>
2025-03-20 11:56:02 -0500

    dsfsd

--- project/globalData.gd
+++ project/globalData.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var ticketNum = 1
+
 var doughs = ["Regular", "Naan", "Paratha"]
 var curries = ["Paneer", "Gobi", "Butter Chicken"]
 var times = ["20 Seconds", "40 Seconds", "60 Seconds"]
--- project/ticket.gd
+++ project/ticket.gd
@@ -4,11 +4,15 @@ extends Node2D
 @onready var dough = $dough
 @onready var curry = $curry
 @onready var time = $time
+@onready var randomNum = -1
 
 
 func _on_ready() -> void:
 	ticketNumber.text = str(globalData.ticketNum)
-	dough.text = str(globalData.doughs[0])
-	curry.text = str(globalData.curries[0])
-	time.text = str(globalData.times[0])
+	randomNum = randi() %3
+	dough.text = str(globalData.doughs[randomNum])
+	randomNum = randi() %3
+	curry.text = str(globalData.curries[randomNum])
+	randomNum = randi() %3
+	time.text = str(globalData.times[randomNum])
 	
