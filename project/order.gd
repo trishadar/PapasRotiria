e04937b3ceca18596f1a248c79e19d17966e7e94
trishadar <trisha.darure@gmail.com>
2025-03-20 12:21:29 -0500

    Update order.gd

--- project/order.gd
+++ project/order.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @onready var ticket = $ticket
 @onready var help = $help
+@onready var orderButton = $orderButton
+@onready var rollButton = $rollButton
+@onready var cookButton = $cookButton
+@onready var curryButton = $curryButton
 
 func _on_order_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://order.tscn")
@@ -23,6 +27,22 @@ func _on_take_order_button_pressed() -> void:
 	ticket.visible = true
 	globalData.ticketOccupied = true
 
-
 func _on_ready() -> void:
 	help.text = globalData.helpText
+	
+func _process(delta):
+	if orderButton.is_hovered():
+		globalData.helpText = "**Order Station**"
+		help.text = globalData.helpText
+	elif rollButton.is_hovered():
+		globalData.helpText = "**Roll Station**"
+		help.text = globalData.helpText
+	elif cookButton.is_hovered():
+		globalData.helpText = "**Cook Station**"
+		help.text = globalData.helpText
+	elif curryButton.is_hovered():
+		globalData.helpText = "**Curry Station**"
+		help.text = globalData.helpText
+	else:
+		globalData.helpText = "..."
+		help.text = globalData.helpText
