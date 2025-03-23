93d0f72b5f8ffdf4669c770e6e471d908d2bcabd
trisha <trisha.darure@gmail.com>
2025-03-23 18:13:49 -0500

    dsadas

--- project/curry.gd
+++ project/curry.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+func change_to_order() -> void:
+	get_tree().change_scene_to_file("res://order.tscn")
+
+func change_to_roll() -> void:
+	get_tree().change_scene_to_file("res://roll.tscn")
+
+func change_to_cook() -> void:
+	get_tree().change_scene_to_file("res://cook.tscn")
+
+func change_to_curry() -> void:
+	get_tree().change_scene_to_file("res://curry.tscn")
--- project/order.gd
+++ project/order.gd
@@ -18,14 +18,11 @@ var currentTicket
 func change_to_order() -> void:
 	get_tree().change_scene_to_file("res://order.tscn")
 
-
 func change_to_roll() -> void:
 	get_tree().change_scene_to_file("res://roll.tscn")
 
-
 func change_to_cook() -> void:
 	get_tree().change_scene_to_file("res://cook.tscn")
-
 
 func change_to_curry() -> void:
 	get_tree().change_scene_to_file("res://curry.tscn")
