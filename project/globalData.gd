19cd30151e7e1be9c002263e0310550bc14caf41
Kkartchner <Kylekartchner2006@gmail.com>
2025-03-24 12:45:42 -0500

    ROTI DRAG!

--- project/globalData.gd
+++ project/globalData.gd
@@ -6,3 +6,5 @@ var doughs = ["Regular", "Naan", "Paratha"]
 var curries = ["Paneer", "Gobi", "Butter Chicken"]
 var times = ["20 Seconds", "40 Seconds", "60 Seconds"]
 var helpText = "..."
+
+var is_dragging = false
