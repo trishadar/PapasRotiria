b6c0bc8b0c10dd10b86d53d4f81fb151419afb4f
Kkartchner <Kylekartchner2006@gmail.com>
2025-03-25 11:51:43 -0500

    1

--- project/globalData.gd
+++ project/globalData.gd
@@ -1,10 +1,44 @@
 extends Node
 
-var ticketNum = 1
+var gameStarted = false
+var ticketNum = 0
 var ticketOccupied = false
 var doughs = ["Regular", "Naan", "Paratha"]
 var curries = ["Paneer", "Gobi", "Butter Chicken"]
 var times = ["20 Seconds", "40 Seconds", "60 Seconds"]
 var helpText = "..."
 
-var is_dragging = false
+var ticket_scene: PackedScene = preload("res://ticket.tscn")
+var canTakeOrder = true
+
+var viewingTicket = null
+var allTickets = []
+
+var ladleMoving = true
+var is_dragging = false;
+
+
+func _ready() -> void:
+	ticketNum += 1
+	var ticketNumber = str(ticketNum)
+	var randomNum = randi() %3
+	var dough = str(doughs[randomNum])
+	randomNum = randi() %3
+	var curry = str(curries[randomNum])
+	randomNum = randi() %3
+	var time = str(times[randomNum])
+	
+	var ticket_data = {
+		"ticketNumber": ticketNumber,
+		"dough": dough,
+		"curry": curry,
+		"time": time
+	}
+	
+	allTickets.append(ticket_data)
+
+func _process(delta: float) -> void:
+	keepRunningFunction()
+	
+func keepRunningFunction() -> void:
+	pass
