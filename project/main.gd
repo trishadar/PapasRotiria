1bf1ede52d06e4b656e4c9ed63c2aa65a71f5481
Kkartchner <Kylekartchner2006@gmail.com>
2025-04-01 11:43:25 -0500

    main

--- project/main.gd
+++ project/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://order.tscn")
+	get_tree().change_scene_to_file("res://main_scene.tscn")
 	globalData.gameStarted = true
 
 
