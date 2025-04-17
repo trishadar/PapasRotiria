2b49f74fee4927a1eac89475fef99d78e45f8da4
trishadar <trisha.darure@gmail.com>
2025-04-17 11:54:07 -0500

    fdfs

--- project/globalData.gd
+++ project/globalData.gd
@@ -14,7 +14,7 @@ var canTakeOrder = false
 var viewingTicket = null
 var allTickets = []
 var pendingTickets = []
-var storage = [0,0,0,0,0,0]
+var storage = [0,0,0,0,0,0,0]
 
 var ladleMoving = true
 var is_dragging = false;
@@ -33,7 +33,7 @@ func _ready() -> void:
 	rng.randomize()
 	
 	timer = Timer.new()
-	timer.wait_time = rng.randi_range(5,10)
+	timer.wait_time = rng.randi_range(1,2)
 	timer.one_shot = false
 	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 	add_child(timer)
@@ -41,7 +41,7 @@ func _ready() -> void:
 	
 func _on_timer_timeout():
 	makeNewTicket()
-	timer.wait_time = rng.randi_range(5,15)
+	timer.wait_time = rng.randi_range(3,5)
 	timer.start()
 
 func _process(delta: float) -> void:
