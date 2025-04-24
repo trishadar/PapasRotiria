0786172584bb85fa4c8f340738cb02b99c995b19
Kkartchner <Kylekartchner2006@gmail.com>
2025-04-24 12:17:15 -0500

    calc

--- project/plate.gd
+++ project/plate.gd
@@ -9,35 +9,38 @@ extends Node2D
 func calculateScore():
 	if(rotiHold.isOccupied):
 		var roti = rotiHold.rotiOccupied
-		print(float(sidebar.viewingTicketNode.timeNum) , " - " , rad_to_deg(roti.cookHandRot))
 		
-		print(250 - abs((float(sidebar.viewingTicketNode.timeNum) - rad_to_deg(roti.cookHandRot))))
+		if(roti.isRoti && sidebar.viewingTicketNode.dough.text == "Roti"):
+			globalData.score += 100
+		
+		if(roti.isNaan && sidebar.viewingTicketNode.dough.text == "Naan"):
+			globalData.score += 100
+		
 		globalData.score += int(250 - abs(float(sidebar.viewingTicketNode.timeNum) - rad_to_deg(roti.cookHandRot)))
 	
 	if(bowlHold.isOccupied):
 		var bowl = bowlHold.rotiOccupied
 		if(bowl.whichCurry == sidebar.viewingTicketNode.curry.text):
 			globalData.score += 50
-			print(50)
 		
 		if(bowl.whichColor == "green"):
 			globalData.score += 100
-			print(100)
+
 		if(bowl.whichColor == "yellow"):
 			globalData.score += 50
-			print(50)
+
 	
 	if(bowlHold2.isOccupied):
 		var bowl = bowlHold2.rotiOccupied
 		if(bowl.whichCurry == sidebar.viewingTicketNode.curry.text):
 			globalData.score += 50
-			print(50)
+
 		
 		if(bowl.whichColor == "green"):
 			globalData.score += 100
-			print(100)
+
 		if(bowl.whichColor == "yellow"):
 			globalData.score += 50
-			print(50)
+
 	
 	globalData.totalScore += globalData.score
