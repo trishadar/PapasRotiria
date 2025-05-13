16e31d4eb25fa2c36fd7f176a45f212f8c913494
Kkartchner <Kylekartchner2006@gmail.com>
2025-05-13 12:45:36 -0500

    gr

--- project/plate.gd
+++ project/plate.gd
@@ -6,41 +6,40 @@ extends Node2D
 
 @onready var sidebar = get_node("/root/MainScene/sidebar")
 
+var rollScore = 0
+var cookScore = 0
+var curryScore = 0
+
 func calculateScore():
 	if(rotiHold.isOccupied):
 		var roti = rotiHold.rotiOccupied
 		
 		if(roti.isRoti && sidebar.viewingTicketNode.dough.text == "Roti"):
-			globalData.score += 100
+			rollScore += 100
+			
 		
 		if(roti.isNaan && sidebar.viewingTicketNode.dough.text == "Naan"):
-			globalData.score += 100
+			rollScore += 100
 		
-		globalData.score += int(250 - abs(float(sidebar.viewingTicketNode.timeNum) - rad_to_deg(roti.cookHandRot)))
+		cookScore += int(250 - abs(float(sidebar.viewingTicketNode.timeNum) - rad_to_deg(roti.cookHandRot)))
 	
 	if(bowlHold.isOccupied):
 		var bowl = bowlHold.rotiOccupied
 		if(bowl.whichCurry == sidebar.viewingTicketNode.curry.text):
-			globalData.score += 50
-		
+			curryScore += 50
 		if(bowl.whichColor == "green"):
-			globalData.score += 100
-
+			curryScore += 100
 		if(bowl.whichColor == "yellow"):
-			globalData.score += 50
-
+			curryScore += 50
 	
 	if(bowlHold2.isOccupied):
 		var bowl = bowlHold2.rotiOccupied
 		if(bowl.whichCurry == sidebar.viewingTicketNode.curry.text):
-			globalData.score += 50
-
-		
+			curryScore += 50
 		if(bowl.whichColor == "green"):
-			globalData.score += 100
-
+			curryScore += 100
 		if(bowl.whichColor == "yellow"):
-			globalData.score += 50
+			curryScore += 50
 
 	
-	globalData.totalScore += globalData.score
+	globalData.totalScore += rollScore + cookScore + curryScore
